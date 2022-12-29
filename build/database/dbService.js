01083ebf924c0e1be390c3d77d055d543e7e51ed
Allison Nunes <allison.nunes@maxmilhas.com.br>
2022-12-29 05:16:09 -0300

    Remove folder

--- build/database/dbService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.database = void 0;
-const node_json_db_1 = require("node-json-db");
-const JsonDBConfig_1 = require("node-json-db/dist/lib/JsonDBConfig");
-const database = new node_json_db_1.JsonDB(new JsonDBConfig_1.Config('database', true, false, '/'));
-exports.database = database;
-//# sourceMappingURL=dbService.js.map
